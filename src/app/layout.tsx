e54189b797dbd2f136f4754818aa52bae373a2b2
Dennis <18040598+data-intelligence-analysis@users.noreply.github.com>
2024-04-06 13:08:56 -0500

    add hover effects and login link in navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
+import './globals.css'
 import MantineSetup from "@/layout/MantineSetup";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import "swiper/css";
