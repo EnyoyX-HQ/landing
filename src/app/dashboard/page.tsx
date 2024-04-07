b4a74a6a564c5a0626ecc74fa70deabf1c223f33
oraclerevolution <mac.n.drix@gmail.com>
2024-04-07 17:26:46 +0000

    removed unused imports

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,35 +1,3 @@
-import Link from "next/link";
-import {
-  Bell,
-  CircleUser,
-  LineChart,
-  Menu,
-  Package,
-  Package2,
-  Search,
-  ShoppingCart, Paperclip, Store,
-  Users,
-} from "lucide-react";
-
-import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { Input } from "@/components/ui/input";
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import "../shadcn.css";
 import DashboardLayout from "./DashboardLayout";
 import Home from "./Home"
