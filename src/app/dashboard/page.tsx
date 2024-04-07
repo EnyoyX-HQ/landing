17dffb8f209a47b7858d84598b0bb77536a13a97
Dennis <18040598+data-intelligence-analysis@users.noreply.github.com>
2024-04-07 14:11:32 -0500

    Update page.tsx
    Signed-off-by: Dennis <18040598+data-intelligence-analysis@users.noreply.github.com>

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,6 @@
 import "../shadcn.css";
 import DashboardLayout from "./DashboardLayout";
-import HomePage from "./home"
+import HomePage from "./Home"
 import Clinic from "./settings/profile/page";
 import Invoice from "./invoices/page";
 import Documentation from "./docs/page";
