306ce761541d23de1483ef16b41bd09bdffee645
Dennis <18040598+data-intelligence-analysis@users.noreply.github.com>
2024-04-10 14:06:56 -0500

    add production prisma logic

--- prisma/client.ts
+++ prisma/client.ts
@@ -16,6 +16,10 @@ declare const global: CustomNodeJsGlobal
 
 const prisma = global.prisma || new PrismaClient()
 
-if (process.env.NODE_ENV === 'development') global.prisma = prisma
+if (process.env.NODE_ENV === 'development'){
+  global.prisma = prisma
+} else {
+  prisma
+}
 
 export default prisma
