02ff5a246c40cfae6144bafca942576677c3cad2
Dennis <18040598+data-intelligence-analysis@users.noreply.github.com>
2024-05-01 15:19:37 -0500

    remove stage

--- src/components/blocks/Announcement/index.tsx
+++ src/components/blocks/Announcement/index.tsx
@@ -6,7 +6,7 @@ const AnnouncementSaleBar = () => {
     <div className={`p-4 bg-ex-deep-green mx-auto max-w-full`}>
       <div className="flex items-center justify-center mx-auto flex-wrap">
         <div className="inline-flex justify-center flex-row gap-4 items-center text-center flex-wrap">
-          <span className='gradient-text text-[12px] sm:text-sm text-center font-bold'>Our platform is currently in Beta stage. Help Us Perfect Your Experience!</span>
+          <span className='gradient-text text-[12px] sm:text-sm text-center font-bold'>Our platform is currently in Beta. Help Us Perfect Your Experience!</span>
           <span className='inline-block'>
             <a href={'#tools'} type='button' className="items-center cursor-pointer border-[1px] rounded-[5px] gradient-text bg-[#171717] py-1 px-5">Learn more</a>
           </span>
