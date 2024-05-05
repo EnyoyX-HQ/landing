ef09c653307f7f4c4322aa90c41ecc59d3e61aa9
Dennis <18040598+data-intelligence-analysis@users.noreply.github.com>
2024-05-05 12:59:12 -0500

    added internationalized navigation

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -1,17 +1,21 @@
 import createMiddleware from 'next-intl/middleware';
+import { localePrefix, locales } from './navigation';
  
 export default createMiddleware({
   // A list of all locales that are supported
-  locales: ['en', 'fr'],
- 
+  //locales: ['en', 'fr'],
+  locales,
   // Used when no locale matches
-  defaultLocale: 'fr'
+  //defaultLocale: 'fr'
+  localePrefix,
+  defaultLocale: 'en'
 });
  
 
 //exclude api routes
 export const config = {
   matcher: '/((?!api|static|.*\\..*|_next).*)'
+  //matcher: ['/((?!api|_next|.*\\..*).*)']
 };
 
 // Match only internationalized pathnames
