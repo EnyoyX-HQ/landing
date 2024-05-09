ec1cc786e0f294e247075eec05966f214618389c
Dennis <18040598+data-intelligence-analysis@users.noreply.github.com>
2024-05-09 12:08:29 -0500

    added gtag to head

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({
   return (
     <html lang={locale}>
       <head>
-        <Script src="https://cdn.weglot.com/weglot.min.js" />
+        {/*<Script src="https://cdn.weglot.com/weglot.min.js" />*/}
         {/*<Script id="show-banner">
           {`Weglot.initialize({
             api_key: 'wg_4f83eed4d5e65923b37d3a12860bd0438'
