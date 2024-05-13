5c54f2af65633fd70720a633fa67ff321a4a164b
Dennis <18040598+data-intelligence-analysis@users.noreply.github.com>
2024-05-13 11:33:08 -0500

    added form validation rules

--- src/app/[locale]/(auth)/contact-us/page.tsx
+++ src/app/[locale]/(auth)/contact-us/page.tsx
@@ -4,6 +4,7 @@ import { ExButton, GlowingBalls } from '@/components'
 import { LogoDark, LogoWhite } from '@/images'
 import { IMaskInput } from 'react-imask';
 import { useMediaQuery } from '@mantine/hooks';
+import { useForm } from '@mantine/form';
 import {
   TextInput,
   PasswordInput,
@@ -83,6 +84,20 @@ const SignUp = () => {
       {item}
     </Combobox.Option>
   ));
+  const form = useForm({
+    mode: 'uncontrolled',
+    validateInputOnChange: true,
+    initialValues: { fname: '', lname: '', company: '', email: '', number: '' },
+
+    // functions will be used to validate values at corresponding key
+    validate: {
+      fname: (value: string) => (value.length < 2 ? 'First name must have at least 2 letters' : null),
+      lname: (value: string) => (value.length < 2 ? 'Last name must have at least 2 letters' : null),
+      company: (value: string) => (value.length < 2 ? 'Company name must have at least 2 letters' : null),
+      email: (value: string) => (/^\S+@\S+$/.test(value) ? null : 'Invalid email'),
+      number: (value: string) => (/^[0-9\-\(\)\s\+]+$/.test(value) ? null : 'Invalid phone number')
+    },
+  });
   return (
     <div className='h-full md:h-screen grid grid-cols-1 md:grid-cols-2'>
       <div className='auth-section hidden md:flex items-center justify-center'>
@@ -107,140 +122,148 @@ const SignUp = () => {
             alt='logo'
           />
         </Link>
+        <form onSubmit={form.onSubmit(console.log)}>
+          <Paper p={10} mt={30}>
+            <Group grow mb='md' mt='md'>
+              <TextInput required label="First name" placeholder='First name' key={form.key('fname')} {...form.getInputProps('fname')}/>
+              <TextInput required label="Last name" placeholder='Last name' key={form.key('lname')} {...form.getInputProps('lname')}/>
+            </Group>
+            <Group grow mb='md' mt='md'>
+              <TextInput required label= "Company name" placeholder='Company' key={form.key('company')} {...form.getInputProps('company')} />
+              <TextInput required label="Email address" placeholder='Email address' key={form.key('email')} {...form.getInputProps('email')} />
+            </Group>
+            <Group grow mb='md' mt='md'>
+              <Combobox
+                store={combobox}
+                onOptionSubmit={(val) => {
+                  setValue(val);
+                  combobox.closeDropdown();
+                }}
+              >
+                <Combobox.Target>
+                  <InputBase
+                    label="Country"
+                    component="button"
+                    type="button"
+                    pointer
+                    rightSection={<Combobox.Chevron />}
+                    rightSectionPointerEvents="none"
+                    onClick={() => combobox.toggleDropdown()}
+                    required
+                  >
+                    {value || <Input.Placeholder>Select Country</Input.Placeholder>}
+                  </InputBase>
+                </Combobox.Target>
 
-        <Paper p={10} mt={30}>
-          <Group grow mb='md' mt='md'>
-            <TextInput placeholder='First name' required />
-            <TextInput placeholder='Last name' required />
-          </Group>
-          <Group grow mb='md' mt='md'>
-            <TextInput placeholder='Company' required />
-            <TextInput placeholder='Email address' required />
-          </Group>
-          <Group grow mb='md' mt='md'>
-            <Combobox
-              store={combobox}
-              onOptionSubmit={(val) => {
-                setValue(val);
-                combobox.closeDropdown();
-              }}
+                <Combobox.Dropdown>
+                  <Combobox.Options mah={200} style={{ overflowY: 'auto' }}>{countryOptions}</Combobox.Options>
+                </Combobox.Dropdown>
+              </Combobox>
+              {/*<TextInput placeholder='Phone number' required />*/}
+              <InputBase
+                label="Telephone"
+                component={IMaskInput}
+                mask="+255 (000) 000-0000"
+                placeholder="Phone number"
+                key={form.key('number')} 
+                {...form.getInputProps('number')}
+                required
+              />
+            </Group>
+            <Group grow mb='md' mt='md'>
+              <Combobox
+                store={comboboxBusiness}
+                onOptionSubmit={(val) => {
+                  setValueInterest(val);
+                  comboboxBusiness.closeDropdown();
+                }}
+              >
+                <Combobox.Target>
+                  <InputBase
+                    label="Type of Business"
+                    component="button"
+                    type="button"
+                    pointer
+                    rightSection={<Combobox.Chevron />}
+                    rightSectionPointerEvents="none"
+                    onClick={() => comboboxBusiness.toggleDropdown()}
+                    required
+                  >
+                    {valueBusiness || <Input.Placeholder>Type of Business</Input.Placeholder>}
+                  </InputBase>
+                </Combobox.Target>
+
+                <Combobox.Dropdown>
+                  <Combobox.Options mah={200} style={{ overflowY: 'auto' }}>{businessOptions}</Combobox.Options>
+                </Combobox.Dropdown>
+              </Combobox>
+              <Combobox
+                store={comboboxInterests}
+                onOptionSubmit={(val) => {
+                  setValueInterest(val);
+                  comboboxInterests.closeDropdown();
+                }}
+              >
+                <Combobox.Target>
+                  <InputBase
+                    label="How did you hear about us"
+                    component="button"
+                    type="button"
+                    pointer
+                    rightSection={<Combobox.Chevron />}
+                    rightSectionPointerEvents="none"
+                    onClick={() => comboboxInterests.toggleDropdown()}
+                  >
+                    {valueInterest || <Input.Placeholder><p className="text-[13px] leading-[18px] md:text-sm">Source of awareness</p></Input.Placeholder>}
+                  </InputBase>
+                </Combobox.Target>
+
+                <Combobox.Dropdown>
+                  <Combobox.Options mah={200} style={{ overflowY: 'auto' }}>{interestOptions}</Combobox.Options>
+                </Combobox.Dropdown>
+              </Combobox>
+            </Group>
+            <Group grow mb='md' mt='md'>
+              <Textarea
+                size="sm"
+                resize="vertical"
+                placeholder="Tell us about your business"
+                value={valueText}
+                onChange={(event) => setValue(event.currentTarget.value)}
+                autosize
+                minRows={4}
+              />
+            </Group>
+            {/*<PasswordInput
+              label='Password'
+              placeholder='Your password'
+              required
+              mt='md'
+            />*/}
+            <ExButton
+              type='link'
+              href='/dashboard/provider'
+              className='w-full mt-10'
+              isGradient
             >
-              <Combobox.Target>
-                <InputBase
-                  component="button"
-                  type="button"
-                  pointer
-                  rightSection={<Combobox.Chevron />}
-                  rightSectionPointerEvents="none"
-                  onClick={() => combobox.toggleDropdown()}
-                  required
-                >
-                  {value || <Input.Placeholder>Select Country</Input.Placeholder>}
-                </InputBase>
-              </Combobox.Target>
+              {/*Sign up*/}
+              Submit
+            </ExButton>
 
-              <Combobox.Dropdown>
-                <Combobox.Options mah={200} style={{ overflowY: 'auto' }}>{countryOptions}</Combobox.Options>
-              </Combobox.Dropdown>
-            </Combobox>
-            {/*<TextInput placeholder='Phone number' required />*/}
-            <InputBase
-              component={IMaskInput}
-              mask="+255 (000) 000-0000"
-              placeholder="Phone number"
-            />
-          </Group>
-          <Group grow mb='md' mt='md'>
-            <Combobox
-              store={comboboxBusiness}
-              onOptionSubmit={(val) => {
-                setValueInterest(val);
-                comboboxBusiness.closeDropdown();
-              }}
-            >
-              <Combobox.Target>
-                <InputBase
-                  component="button"
-                  type="button"
-                  pointer
-                  rightSection={<Combobox.Chevron />}
-                  rightSectionPointerEvents="none"
-                  onClick={() => comboboxBusiness.toggleDropdown()}
-                  required
-                >
-                  {valueBusiness || <Input.Placeholder>Type of Business</Input.Placeholder>}
-                </InputBase>
-              </Combobox.Target>
-
-              <Combobox.Dropdown>
-                <Combobox.Options mah={200} style={{ overflowY: 'auto' }}>{businessOptions}</Combobox.Options>
-              </Combobox.Dropdown>
-            </Combobox>
-            <Combobox
-              store={comboboxInterests}
-              onOptionSubmit={(val) => {
-                setValueInterest(val);
-                comboboxInterests.closeDropdown();
-              }}
-            >
-              <Combobox.Target>
-                <InputBase
-                  component="button"
-                  type="button"
-                  pointer
-                  rightSection={<Combobox.Chevron />}
-                  rightSectionPointerEvents="none"
-                  onClick={() => comboboxInterests.toggleDropdown()}
-                >
-                  {valueInterest || <Input.Placeholder><p className="text-[13px] leading-[18px] md:text-sm">Source of awareness</p></Input.Placeholder>}
-                </InputBase>
-              </Combobox.Target>
-
-              <Combobox.Dropdown>
-                <Combobox.Options mah={200} style={{ overflowY: 'auto' }}>{interestOptions}</Combobox.Options>
-              </Combobox.Dropdown>
-            </Combobox>
-          </Group>
-          <Group grow mb='md' mt='md'>
-            <Textarea
-              size="sm"
-              resize="vertical"
-              placeholder="Tell us about your business"
-              value={valueText}
-              onChange={(event) => setValue(event.currentTarget.value)}
-              autosize
-              minRows={4}
-            />
-          </Group>
-          {/*<PasswordInput
-            label='Password'
-            placeholder='Your password'
-            required
-            mt='md'
-          />*/}
-          <ExButton
-            type='link'
-            href='/dashboard/provider'
-            className='w-full mt-10'
-            isGradient
-          >
-            {/*Sign up*/}
-            Submit
-          </ExButton>
-
-          <Text c='dimmed' size='sm' ta='center' mt={40}>
-            Already have an account?{' '}
-            <Anchor
-              onClick={() => router.push('/login')}
-              size='sm'
-              c={'green'}
-              fw={'bold'}
-              component='button'
-            >
-              Sign in
-            </Anchor>
-          </Text>
-        </Paper>
+            <Text c='dimmed' size='sm' ta='center' mt={40}>
+              Already have an account?{' '}
+              <Anchor
+                onClick={() => router.push('/login')}
+                size='sm'
+                c={'green'}
+                fw={'bold'}
+                component='button'
+              >
+                Sign in
+              </Anchor>
+            </Text>
+          </Paper>
+        </form>
       </Container>
     </div>
   )
