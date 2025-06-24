f3e2ad592006b3590c53d827e10d4e505f0085f1
wearewob <wearewob@gmail.com>
2025-06-24 17:49:41 +0100

    Updated HeroSection.tsx

--- components/screens/Insurance/sections/HeroSection/HeroSection.tsx
+++ components/screens/Insurance/sections/HeroSection/HeroSection.tsx
@@ -19,7 +19,7 @@ export const HeroSection = (): JSX.Element => {
           </div>
           <div className="inline-flex justify-start items-center gap-3">
             <div className="flex justify-start items-start gap-3">
-              <div className="h-14 px-6 py-3 bg-lime- rounded-[100px] flex justify-center items-center gap-3 overflow-hidden">
+              <div className="h-14 px-6 py-3 bg-lime-100 rounded-[100px] flex justify-center items-center gap-3 overflow-hidden">
                 <Button className="all-[unset] justify-start text-gray-900 text-base font-medium font-['Neue_Montreal'] capitalize leading-normal">
                   See our health plans
                 </Button>
