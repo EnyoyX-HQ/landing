acfabce36952896b0a0a5c9093d177aa699593ed
maven harry <mavenharry@gmail.com>
2025-07-23 22:37:58 +0100

    chore: fix failing build

--- components/shared/index.ts
+++ components/shared/index.ts
@@ -1,9 +1,3 @@
-// Screen exports
-export { Home } from "./Home";
-export { ForBanksFinancial } from "./ForBanksFinancial";
-export { Insurance } from "./Insurance";
-
-// Shared component exports
 export { Footer } from "./Footer";
 export { Navigation } from "./Navigation";
 export { ImageWithFallback } from "./ImageWithFallback";
